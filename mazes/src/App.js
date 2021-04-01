b24cb180fc55b15ffb54b0ab351fda7beb15f9ff
Ayelet Danieli <ayeletd@gmail.com>
2021-04-01 14:10:17 +0300

    hello world

--- mazes/src/App.js
+++ mazes/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Hi, you've reached the image maker</h1>
+        <p>Here Shall be a project about image processing</p>
+      </main>
     </div>
   );
 }
